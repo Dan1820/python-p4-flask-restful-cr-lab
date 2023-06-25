4abb88899a09a3288b0f05c8648c9f34d28f5558
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2023-06-25 19:11:25 +0300

    done

--- server/app.py
+++ server/app.py
@@ -17,10 +17,62 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        plant_list=[]
+        for plant in plants:
+            plant_list.append({
+                'id': plant.id,
+                'name':plant.name,
+                'name':plant.name,
+                'image':plant.image,
+                'price':float(plant.price)
+            })
+
+        return jsonify(plant_list)
+    
+    def post(self):
+        data= request.get_json()
+        name=data.get('name')
+        image=data.get('image')
+        price=data.get('price')
+
+        if not name or not image or not price:
+            return make_response(jsonify({'errror': 'Missing required fields'}),400)
+        plant = Plant(name=name,image=image,price=float(price))
+        db.session.add(plant)
+        db.session.commit()
+        return  jsonify({
+            'id': plant.id,
+            'name':plant.name,
+            'name':plant.name,
+            'image':plant.image,
+            'price':float(plant.price)
+            
+        }),201
+        
+            
 
 class PlantByID(Resource):
-    pass
+    def get (self,plant_id):
+        plant = Plant.query.get(plant_id)
+
+        if not plant:
+            return make_response(jsonify({'error': 'Plant not found'}),404)
+        
+        plant_data={
+            'id': plant.id,
+            'name':plant.name,
+            'name':plant.name,
+            'image':plant.image,
+            'price':float(plant.price)
+
+        }
+        return jsonify(plant_data)
+    
+    api.add_resource(Plants,'/plants')
+    api.add_resource(PlantByID,'/plants/<int:plant_id>')
+
         
 
 if __name__ == '__main__':
